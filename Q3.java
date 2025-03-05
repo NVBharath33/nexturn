2ceb7d1ae2ebcd91cde314711d28d166690fbd6a
NVBharath33 <nvbharath3103@gmail.com>
2025-03-05 15:56:14 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,15 @@
+package Day3;
+
+public class Q3 {
+        public static void main(String[] args) {
+            for (int i = 1; i <= 3; i++) {  // Loop for 1, 2, 3
+                System.out.println("" + i);  // Print the number with spaces
+    
+                for (int j = 1; j <= 4; j++) {  // Loop to print 1 2 3 4
+                    System.out.print(j + " ");
+                }
+                System.out.println();  // Move to next line
+            }
+        }
+    }
+    
---
+++ ScannerEx.java
@@ -0,0 +1,18 @@
+package Day3;
+import java.util.Scanner;
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+public class ScannerEx {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your name:");
+        String name=sc.nextLine();
+        System.out.println(name);
+    }
+}
+
+
+
